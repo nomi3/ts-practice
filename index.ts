e77c274ec7af885cf3b5c766790ffe40a28664bb
nomi3 <thisisnomi3@gmail.com>
2022-10-12 00:11:07 +0900

    add some kind of types

--- index.ts
+++ index.ts
@@ -4,3 +4,35 @@ let float: number = 3.14;
 let negative: number = -0.12;
 let single: string = 'hello';
 
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Jack',
+  age: 21
+};
+
+const fruits: string[] = ['apple', 'banana'];
+
+const book: [string, number, boolean] = ['bussiness', 1500, false];
+
+enum CoffeeSize {
+  SHORT = 'SHORT',
+  TALL = 'TALL',
+  GRANDE = 'GRANDE',
+  VENTI = 'VENTI'
+}
+
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL
+}
+
+let anything: any = true;
+
+let unionType: number | string = 10;
+let unionTypes: (number | string)[] = [10, 'hello'];
+
+const apple = 'apple';
+
+let clothSize: 'small' | 'medium' | 'large' = 'large';

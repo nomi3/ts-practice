bf18c4878e7e67cdd6336c20855aac3ca7db0ef5
nomi3 <thisisnomi3@gmail.com>
2022-10-15 21:24:00 +0900

    add class

---
+++ src/class.ts
@@ -0,0 +1,20 @@
+class Person {
+  name: string;
+  constructor(initName: string) {
+    this.name = initName;
+  }
+
+  greeting(this: { name: string }) {
+    console.log(`Hello I am ${this.name}`);
+  }
+}
+
+const quill = new Person('Quill');
+quill.greeting();
+
+const anotherQuill = {
+  name: 'anotherQuill',
+  anotherGreeting: quill.greeting
+}
+
+anotherQuill.anotherGreeting();

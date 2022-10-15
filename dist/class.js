1d55d8e5d0b042589320e878bf2ebf20573a750d
nomi3 <thisisnomi3@gmail.com>
2022-10-15 22:09:35 +0900

    add abstract instance

--- dist/class.js
+++ dist/class.js
@@ -1,16 +1,43 @@
 "use strict";
 class Person {
-    constructor(initName) {
-        this.name = initName;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    static isAdult(age) {
+        if (age > 17)
+            return true;
+        return false;
+    }
+    incrementAge() {
+        this.age += 1;
     }
     greeting() {
-        console.log(`Hello I am ${this.name}`);
+        console.log(`Hello I am ${this.name}. I am ${this.age} years old.`);
+        this.explainJob();
     }
 }
-const quill = new Person('Quill');
-quill.greeting();
-const anotherQuill = {
-    name: 'anotherQuill',
-    anotherGreeting: quill.greeting
-};
-anotherQuill.anotherGreeting();
+Person.species = 'Homo sapiens';
+class Teacher extends Person {
+    constructor(name, age, _subject) {
+        super(name, age);
+        this._subject = _subject;
+    }
+    explainJob() {
+        console.log(`I am a teacher. I teach ${this.subject}.`);
+    }
+    get subject() {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject.');
+        }
+        this._subject = value;
+    }
+}
+const teacher = new Teacher('Quill', 38, 'Math');
+teacher.greeting();
--- src/class.ts
+++ src/class.ts
@@ -1,20 +1,44 @@
-class Person {
-  name: string;
-  constructor(initName: string) {
-    this.name = initName;
+abstract class Person {
+  static species = 'Homo sapiens';
+  static isAdult(age: number) {
+    if (age > 17) return true;
+    return false;
+  }
+  constructor(public name: string, protected age: number) {}
+
+  incrementAge() {
+    this.age += 1;
+  }
+  greeting(this: Person) {
+    console.log(`Hello I am ${this.name}. I am ${this.age} years old.`);
+    this.explainJob();
+  }
+  abstract explainJob(): void;
+}
+
+class Teacher extends Person {
+  explainJob(): void {
+    console.log(`I am a teacher. I teach ${this.subject}.`)
   }
 
-  greeting(this: { name: string }) {
-    console.log(`Hello I am ${this.name}`);
+  get subject() {
+    if (!this._subject) {
+      throw new Error('There is no subject.');
+    }
+    return  this._subject;
+  }
+
+  set subject(value) {
+    if (!value) {
+      throw new Error('There is no subject.');
+    }
+    this._subject = value;
+  }
+
+  constructor(name: string, age: number, private _subject: string) {
+    super(name, age);
   }
 }
 
-const quill = new Person('Quill');
-quill.greeting();
-
-const anotherQuill = {
-  name: 'anotherQuill',
-  anotherGreeting: quill.greeting
-}
-
-anotherQuill.anotherGreeting();
+const teacher = new Teacher('Quill', 38, 'Math');
+teacher.greeting();

83ca50b13f82c5f2099fc8fa370539e0935890f1
nomi3 <thisisnomi3@gmail.com>
2022-10-23 10:23:10 +0900

    add decorator

--- dist/decorator.js
+++ dist/decorator.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logging(message) {
     return function (constructor) {
         console.log(message);
@@ -13,22 +16,56 @@ function Logging(message) {
 }
 function Component(template, selector) {
     return function (constructor) {
-        const mountedElement = document.querySelector(selector);
-        const instance = new constructor();
-        if (mountedElement) {
-            mountedElement.innerHTML = template;
-            mountedElement.querySelector('h1').textContent = instance.name;
-        }
+        return class extends constructor {
+            constructor(...args) {
+                super(...args);
+                const mountedElement = document.querySelector(selector);
+                const instance = new constructor();
+                if (mountedElement) {
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector('h1').textContent = instance.name;
+                }
+            }
+        };
     };
 }
+function PropertyLogging(target, propetyKey) {
+    console.log('key');
+    console.log(target);
+    console.log(propetyKey);
+}
+function MethodLogging(target, propetyKey, descriptor) {
+    console.log('method');
+    console.log(target);
+    console.log(propetyKey);
+    console.log(descriptor);
+}
+function ParameterLogging(target, propetyKey, parameterIndex) {
+    console.log('parameter');
+    console.log(target);
+    console.log(propetyKey);
+    console.log(parameterIndex);
+}
 let User = class User {
-    constructor() {
+    constructor(age) {
+        this.age = age;
         this.name = 'Quill';
         console.log('aaa');
     }
+    greeting(message) {
+        console.log(message);
+    }
 };
+User.name2 = 'Quill';
+__decorate([
+    MethodLogging,
+    __param(0, ParameterLogging)
+], User.prototype, "greeting", null);
+__decorate([
+    PropertyLogging
+], User, "name2", void 0);
 User = __decorate([
     Component('<h1>{{name}}</h1>', '#app'),
     Logging('Logging user')
 ], User);
-const user1 = new User();
+const user1 = new User(32);
--- src/decorator.ts
+++ src/decorator.ts
@@ -6,23 +6,57 @@ function Logging(message: string) {
 }
 
 function Component(template: string, selector: string) {
-  return function (constructor: {new(...args: any[]): {name: string}}) {
-    const mountedElement = document.querySelector(selector);
-    const instance = new constructor();
-    if (mountedElement) {
-      mountedElement.innerHTML = template;
-      mountedElement.querySelector('h1')!.textContent = instance.name;
+  return function<T extends {new(...args: any[]): {name: string}}>(constructor: T) {
+    return class extends constructor {
+      constructor(...args: any[]) {
+        super(...args);
+        const mountedElement = document.querySelector(selector);
+        const instance = new constructor();
+        if (mountedElement) {
+          mountedElement.innerHTML = template;
+          mountedElement.querySelector('h1')!.textContent = instance.name;
+        }
+      }
     }
   }
+}
+
+function PropertyLogging(target: any, propetyKey: string) {
+  console.log('key');
+  console.log(target);
+  console.log(propetyKey);
+
+}
+
+function MethodLogging(target: any, propetyKey: string, descriptor: PropertyDescriptor) {
+  console.log('method');
+  console.log(target);
+  console.log(propetyKey);
+  console.log(descriptor);
+
+}
+
+function ParameterLogging(target: any, propetyKey: string, parameterIndex: number) {
+  console.log('parameter');
+  console.log(target);
+  console.log(propetyKey);
+  console.log(parameterIndex);
+
 }
 
 @Component('<h1>{{name}}</h1>', '#app')
 @Logging('Logging user')
 class User {
+  @PropertyLogging
+  static name2 = 'Quill';
   name = 'Quill';
-  constructor() {
+  constructor(public age: number) {
     console.log('aaa');
+  }
+  @MethodLogging
+  greeting(@ParameterLogging message: string) {
+    console.log(message);
   }
 }
 
-const user1 = new User();
+const user1 = new User(32);

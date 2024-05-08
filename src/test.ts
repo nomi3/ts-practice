8a6f5f08377cce17734cfcde7b8a0ff938278f83
nomi3 <thisisnomi3@gmail.com>
2024-05-08 14:55:27 +0900

    filterdの設定

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,25 @@
-const array = [1, 2, 3, 4, 5];
-const newArray = array.filter((value) => {
-  return value > 3;
+interface CircleStat {
+  id: string;
+  name: string;
+  count: number;
+}
+
+const id: string[] = ["1", "2", "3", "4", "5"];
+const filteredId: string[] = id.filter((value: string) => {
+  return value !== "3";
 });
 
-console.log(newArray);
+const circles: CircleStat[] = id.map((value: string) => {
+  return {
+    id: value,
+    name: `name-${value}`,
+    count: Math.random() * 100,
+  };
+});
+console.log(circles);
+
+const filteredCircles: CircleStat[] = circles.filter(({ id }) => {
+  return filteredId.includes(id);
+});
+
+console.log(filteredCircles);
